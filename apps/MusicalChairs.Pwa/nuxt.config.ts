92f5321e00726c6d91e1e4acb104c4eb1cbbad3e
Gorbo <sam.I.gorbatov@gmail.com>
2024-10-11 22:13:21 +1100

    (wip) copied manifest

--- apps/MusicalChairs.Pwa/nuxt.config.ts
+++ apps/MusicalChairs.Pwa/nuxt.config.ts
@@ -5,5 +5,39 @@ export default defineNuxtConfig({
     modules: ['@nuxtjs/ionic', '@vite-pwa/nuxt'],
     pwa: {
         registerWebManifestInRouteRules: true,
+        client: {
+            installPrompt: true,
+        },
+        strategies: 'injectManifest',
+        registerType: 'autoUpdate',
+        manifest: {
+            name: 'Musical Chairs',
+            short_name: 'MusicalChairs',
+            theme_color: '#ffffff',
+            icons: [
+                {
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+            ],
+        },
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        },
+        injectManifest: {
+            globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        },
     },
 })
