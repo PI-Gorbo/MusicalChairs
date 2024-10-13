0b1a50e7971251ea54254062c2f592690deb838d
Gorbo <sam.I.gorbatov@gmail.com>
2024-10-13 11:13:07 +1100

    (wip) changed to generate over build

--- apps/MusicalChairs.Pwa/app.vue
+++ apps/MusicalChairs.Pwa/app.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div>
-    <VitePwaManifest />
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+        <VitePwaManifest />
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+    </div>
 </template>
