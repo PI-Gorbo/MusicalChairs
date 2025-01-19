b5843ee1338195d67fec2d5d186a2260ab815d8b
SamLaptop <sam.gorbwork@gmail.com>
2025-01-19 12:58:07 +1100

    wip: Added all shadcn components

--- src/MusicalChairs.Pwa/pages/index.vue
+++ src/MusicalChairs.Pwa/pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-    <IonPage> 
-        <IonButton @click="clickData.refresh">
-            {{ clickData.data ?? 'UNKNOWN' }}
-        </IonButton> </IonPage>
+  <IonPage>
+    <IonButton @click="clickData.refresh">
+      {{ clickData.data ?? 'UNKNOWN' }}
+    </IonButton>
+  </IonPage>
 </template>
 <script setup lang="ts">
-import { count } from '~/utils/generated/Library';
+import {count} from '~/utils/generated/Library';
 
 const clickData = useAsyncData(count)
 
