84fceaf02777a2658e73e0d1276b65609c912f25
SamLaptop <sam.gorbwork@gmail.com>
2025-01-31 03:14:40 +1100

    wip: Added back PWA

---
+++ src/MusicalChairs.Pwa/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <IonPage>
+    <IonButton @click="clickData.refresh">
+      {{ clickData.data ?? 'UNKNOWN' }}
+    </IonButton>
+  </IonPage>
+</template>
+<script setup lang="ts">
+import {count} from '~/utils/generated/Library';
+
+const clickData = useAsyncData(count)
+
+
+</script>
