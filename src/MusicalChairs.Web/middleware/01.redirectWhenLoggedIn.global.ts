eba66e028fd6aa85eb9ec22a720e8abd2826e345
Gorbo <sam.I.gorbatov@gmail.com>
2025-03-02 11:10:25 +1100

    wip: Added back widdleware

--- src/MusicalChairs.Web/middleware/01.redirectWhenLoggedIn.global.ts
+++ src/MusicalChairs.Web/middleware/01.redirectWhenLoggedIn.global.ts
@@ -1,13 +1,13 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 
-    // console.log("01.redirectWhenLoggedIn.global.ts");
-    // if (to.name == 'index' || to.meta.requiresAuth) return;
+    console.log("01.redirectWhenLoggedIn.global.ts");
+    if (to.name == 'index' || to.meta.requiresAuth) return;
 
-    // const userStore = useUserStore();
-    // const isLoggedIn = await userStore.isLoggedIn();
-    // if (isLoggedIn) {
-    //     return navigateTo({
-    //         path: "/home",
-    //     });
-    // }
+    const userStore = useUserStore();
+    const isLoggedIn = await userStore.isLoggedIn();
+    if (isLoggedIn) {
+        return navigateTo({
+            path: "/app/home",
+        });
+    }
 });
--- src/MusicalChairs.Web/middleware/02.checkAuth.global.ts
+++ src/MusicalChairs.Web/middleware/02.checkAuth.global.ts
@@ -1,21 +1,20 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 
-    // console.log("02.checkAuth.global.ts");
-    // debugger;
-    // if (!to.meta.requiresAuth) return;
+    console.log("02.checkAuth.global.ts");
+    if (!to.meta.requiresAuth) return;
 
-    // const userStore = useUserStore();
-    // const isLoggedIn = await userStore.isLoggedIn();
-    // if (isLoggedIn) {
-    //     return;
-    // }
+    const userStore = useUserStore();
+    const isLoggedIn = await userStore.isLoggedIn();
+    if (isLoggedIn) {
+        return;
+    }
 
-    // // If the user is not logged in, return them to the 'does not require auth zone'
-    // return navigateTo({
-    //     path: "/login",
-    //     query: {
-    //         redirectTo: to.path,
-    //     },
-    // });
+    // If the user is not logged in, return them to the 'does not require auth zone'
+    return navigateTo({
+        path: "/login",
+        query: {
+            redirectTo: to.path,
+        },
+    });
 
 });
